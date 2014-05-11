9c790cea901015d84fce9c73284379736c6b369b
Paddy Mullen <paddy@paddymullen.com>
2014-05-11 10:20:18 -0400

    bokeh jammed in

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,6 +4,49 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
+      var r = new Bokeh.Random(123456789);
+
+
+      var x = _.map(_.range(4000), function () {return r.randf()*100;});
+      var y = _.map(_.range(4000), function () {return r.randf()*100;});
+
+      var color = _.map(_.zip(x,y), function (val){
+          var r = (Math.floor(50+2*val[0])).toString();
+          var g =  (Math.floor(30+2*val[1])).toString();
+          return "rgb(" + r + "," + g + ",150)";});
+     
+      var data = {
+          x: x,
+          y: y,
+          radius: _.map(_.range(4000), function () {return r.randf()+0.3;}),
+          color: color
+      };
+
+      var scatter = {
+          type: 'circle',
+          x: 'x',
+          y: 'y',
+          radius: 'radius',
+          radius_units: 'data',
+          fill_color: 'color',
+          fill_alpha: 0.6,
+          line_color: null
+      };
+
+      var options = {
+          title: "Scatter Demo",
+          dims: [600, 600],
+          xrange: [0, 100],
+          yrange: [0, 100],
+          xaxes: "min",
+          yaxes: "min",
+          tools: true,
+          legend: false
+      };
+
+      var plot = Bokeh.Plotting.make_plot(scatter, data, options);
+      Bokeh.Plotting.show(plot);
+
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
