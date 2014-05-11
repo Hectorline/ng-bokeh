550368600dbc82b1c447c31429819d0b3f53bf54
Paddy Mullen <paddy@paddymullen.com>
2014-05-11 10:45:36 -0400

    very basic bokeh plot directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,52 +3,76 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-      var r = new Bokeh.Random(123456789);
+    .controller('MyCtrl1', ['$scope', function($scope) {
+        var r = new Bokeh.Random(123456789);
 
 
-      var x = _.map(_.range(4000), function () {return r.randf()*100;});
-      var y = _.map(_.range(4000), function () {return r.randf()*100;});
+        var x = _.map(_.range(4000), function () {return r.randf()*100;});
+        var y = _.map(_.range(4000), function () {return r.randf()*100;});
 
-      var color = _.map(_.zip(x,y), function (val){
-          var r = (Math.floor(50+2*val[0])).toString();
-          var g =  (Math.floor(30+2*val[1])).toString();
-          return "rgb(" + r + "," + g + ",150)";});
-     
-      var data = {
-          x: x,
-          y: y,
-          radius: _.map(_.range(4000), function () {return r.randf()+0.3;}),
-          color: color
-      };
+        var color = _.map(_.zip(x,y), function (val){
+            var r = (Math.floor(50+2*val[0])).toString();
+            var g =  (Math.floor(30+2*val[1])).toString();
+            return "rgb(" + r + "," + g + ",150)";});
+        
+        $scope.outer_data = {
+            x: x,
+            y: y,
+            radius: _.map(_.range(4000), function () {return r.randf()+0.3;}),
+            color: color
+        };
+        $scope.outer_renderers = [{
+            type: 'circle',
+            x: 'x',
+            y: 'y',
+            radius: 'radius',
+            radius_units: 'data',
+            fill_color: 'color',
+            fill_alpha: 0.6,
+            line_color: null
+        }];
 
-      var scatter = {
-          type: 'circle',
-          x: 'x',
-          y: 'y',
-          radius: 'radius',
-          radius_units: 'data',
-          fill_color: 'color',
-          fill_alpha: 0.6,
-          line_color: null
-      };
-
-      var options = {
-          title: "Scatter Demo",
-          dims: [600, 600],
-          xrange: [0, 100],
-          yrange: [0, 100],
-          xaxes: "min",
-          yaxes: "min",
-          tools: true,
-          legend: false
-      };
-
-      var plot = Bokeh.Plotting.make_plot(scatter, data, options);
-      Bokeh.Plotting.show(plot);
+        $scope.outer_options = {
+            title: "Scatter Demo",
+            dims: [600, 600],
+            xrange: [0, 100],
+            yrange: [0, 100],
+            xaxes: "min",
+            yaxes: "min",
+            tools: true,
+            legend: false
+        };
+        $scope.make_blue = function () {
+            $scope.outer_renderers = [{
+                type: 'circle',
+                x: 'x',
+                y: 'y',
+                radius: 'radius',
+                radius_units: 'data',
+                fill_color: 'blue',
+                fill_alpha: 0.6,
+                line_color: null
+            }];
+        };
 
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+        $scope.make_red = function () {
+            $scope.outer_renderers = [{
+                type: 'circle',
+                x: 'x',
+                y: 'y',
+                radius: 'radius',
+                radius_units: 'data',
+                fill_color: 'red',
+                fill_alpha: 0.6,
+                line_color: null
+            }];
+        };
 
-  }]);
+
+
+
+    }])
+    .controller('MyCtrl2', ['$scope', function($scope) {
+
+    }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -4,8 +4,37 @@
 
 
 angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
+    directive('appVersion', ['version', function(version) {
+        return function(scope, elm, attrs) {
+            elm.text(version);
+        };
+    }])
+    .directive('bokehPlot', function() {
+        return {
+            scope: {
+                data:'=',
+                renderers:'=',
+                options:'=',
+
+            },
+            restrict: 'AE',
+            replace: true,
+            template: '<div class="bokeh-plot-directive-root"></div>',
+            link: function(scope, elem, attrs) {
+                var makePlot = function (){
+                    console.log(elem.html());
+                    console.log("clear");
+                    elem.html('');
+                    var data = scope.data;
+                    var scatter = scope.renderers[0];
+                    var options = scope.options;
+                    scope.plot = Bokeh.Plotting.make_plot(scatter, data, options);
+                    Bokeh.Plotting.show(scope.plot, elem);
+                };
+                makePlot();
+                scope.$watch('data', makePlot);
+                scope.$watch('renderers', makePlot);
+                scope.$watch('options', makePlot);
+            }
+        };
+    });
